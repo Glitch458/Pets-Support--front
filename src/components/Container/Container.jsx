94c13bd7e9693a78cd60cf0d5bd5ce9fcc0e7d75
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-11-11 23:59:05 +0200

    add props

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,7 +1,7 @@
 import { ContainerDiv } from './Container.styled';
 
-const Container = () => {
-  return <ContainerDiv></ContainerDiv>;
+const Container = ({ children }) => {
+  return <ContainerDiv>{children}</ContainerDiv>;
 };
 
 export default Container;
