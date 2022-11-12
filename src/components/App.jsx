8a310e0cc926d41b0fb19fc0d5a85bc012fb8456
Andrey Katrych <andrew.katrich@gmail.com>
2022-11-12 18:34:03 +0200

    fix imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
-const Container = lazy(() => import('./Container/Container'));
-// const SharedLayout = lazy(() => import('./pages/SharedLayout/SharedLayout'));
-// const Header = lazy(() => import('./pages/Header/Header'));
-// const RegisterPage = lazy(() => import('./pages/RegisterPage/RegisterPage'));
-// const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
-// const OurFriendsPage = lazy(() =>import('./pages/OurFriendsPage/OurFriendsPage'));
-// const NewsPage = lazy(() => import('./pages/NewsPage/NewsPage'));
-const NoticesPage = lazy(() => import('./pages/NoticesPage/NoticesPage'));
-// const UserPage = lazy(() => import('./pages/UserPage/UserPage'));
+const Container = lazy(() => import('components/Container/Container'));
+// const SharedLayout = lazy(() => import('pages/SharedLayout/SharedLayout'));
+// const Header = lazy(() => import('pages/Header/Header'));
+// const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+// const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
+// const OurFriendsPage = lazy(() =>import('pages/OurFriendsPage/OurFriendsPage'));
+// const NewsPage = lazy(() => import('pages/NewsPage/NewsPage'));
+const NoticesPage = lazy(() => import('pages/NoticesPage/NoticesPage'));
+// const UserPage = lazy(() => import('pages/UserPage/UserPage'));
 
 const App = () => {
   return (
