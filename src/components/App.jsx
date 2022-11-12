203154ce80eb8be1015bf00b4919e4eb08ca5c99
Andrey Katrych <andrew.katrich@gmail.com>
2022-11-12 03:22:22 +0200

    comment part of code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import Container from './Container/Container';
 // import SharedLayout from './pages/SharedLayout/SharedLayout';
 // import Header from './pages/Header/Header';
@@ -15,14 +15,14 @@ const App = () => {
       {/* <SharedLayout path="/">
         <Header />
       </SharedLayout> */}
-      <Routes>
-        {/* <Route path="/register" element={<RegisterPage />} />
+      {/* <Routes>
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/friends" element={<OurFriendsPage />} />
         <Route path="/news" element={<NewsPage />} />
         <Route path="/notices/:categoryName" element={<NoticesPage />} />
-        <Route path="/user" element={<UserPage />} /> */}
-      </Routes>
+        <Route path="/user" element={<UserPage />} />
+      </Routes> */}
     </Container>
   );
 };
