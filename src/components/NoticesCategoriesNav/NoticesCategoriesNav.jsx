deb702aac68289ff95585557ceb7aa11321ce5d7
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-11-13 16:17:28 +0200

    added styles in component NotiesCategoriesNav

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -1,40 +1,46 @@
 // import { useSelector } from 'react-redux';
 import {
   NoticesCategoriesNavContainer,
+  CotegiriesButonContainer,
   PrivateUl,
   PublicUl,
   Li,
+  AddButton,
 } from './NoticesCategoriesNav.styled';
 import { Button, AddButtonCircle, AddButtonBasic } from '../Button/Button';
-//import { AddButtonCircle, AddButtonBasic } from '../AddButton/AddButton';
 
 const NoticesCategoriesNav = () => {
   //const { token } = useSelector(state => state.user);
   const publicCategories = ['sell', 'lost/found', 'in godod hands'];
   const privateCategories = ['Favorite ads', 'My adds'];
   const token = 'token';
+
   return (
     <NoticesCategoriesNavContainer>
-      <PublicUl>
-        {publicCategories.map(item => (
-          <Li key={item}>
-            <Button type="button" content={item} />
-          </Li>
-        ))}
-      </PublicUl>
-      <PrivateUl>
-        {token &&
-          privateCategories.map(item => (
+      <CotegiriesButonContainer>
+        <PublicUl>
+          {publicCategories.map(item => (
             <Li key={item}>
-              <Button content={item} />
+              <Button type="button" content={item} />
             </Li>
           ))}
-      </PrivateUl>
-      {window.innerWidth <= 480 ? (
-        <AddButtonCircle type="button" content="Add pet" />
-      ) : (
-        <AddButtonBasic type="button" content="Add pet" />
-      )}
+        </PublicUl>
+        <PrivateUl>
+          {token &&
+            privateCategories.map(item => (
+              <Li key={item}>
+                <Button content={item} />
+              </Li>
+            ))}
+        </PrivateUl>
+      </CotegiriesButonContainer>
+      <AddButton>
+        {window.innerWidth <= 768 ? (
+          <AddButtonCircle type="button" content="Add pet" />
+        ) : (
+          <AddButtonBasic type="button" content="Add pet" />
+        )}
+      </AddButton>
     </NoticesCategoriesNavContainer>
   );
 };
--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.styled.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.styled.jsx
@@ -2,24 +2,46 @@ import styled from 'styled-components';
 
 export const NoticesCategoriesNavContainer = styled.div`
   display: flex;
-  flex-wrap: wrap;
+  justify-content: space-between;
   margin-top: 30px;
   @media screen and (min-width: 768px) {
     margin-top: 40px;
   }
 `;
+export const CotegiriesButonContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  @media screen and (min-width: 1280px) {
+    display: flex;
+    flex-wrap: wrap;
+  }
+`;
 export const PrivateUl = styled.ul`
   display: flex;
+  flex-wrap: wrap;
   margin: 0;
   padding: 0;
 `;
 export const PublicUl = styled.ul`
   display: flex;
+  flex-wrap: wrap;
   margin: 0;
   padding: 0;
 `;
 export const Li = styled.li`
-  display: inline-block;
   margin-right: 15px;
+  margin-bottom: 15px;
   list-style-type: none;
+`;
+export const AddButton = styled.div`
+  @media screen and (max-width: 767px) {
+    position: absolute;
+    top: 50%;
+    right: 0;
+  }
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-shrink: 0;
+    align-self: flex-start;
+  }
 `;
