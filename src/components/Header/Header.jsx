98c6482e768d45f1d30626afa26ee5b697559788
SergDeikun <dsv2807@gmail.com>
2022-11-13 09:55:54 +0200

    add SharedLayout & header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+// import { Logo } from 'components/Logo/Logo';
+
+import { HeaderBox } from './Header.styled';
+
+const Header = () => {
+  return (
+    <>
+      <HeaderBox>
+        <span>hello</span>
+        {/* <Logo /> */}
+        {/* <Nav/> */}
+      </HeaderBox>
+    </>
+  );
+};
+
+export default Header;
