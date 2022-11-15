778ebd18534824af2e5f6988ae29ef0c27bf17d8
Andrey Katrych <andrew.katrich@gmail.com>
2022-11-15 22:09:53 +0200

    link newspage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ const SharedLayout = lazy(() => import('components/SharedLayout/SharedLayout'));
 // const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
 // const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
 // const OurFriendsPage = lazy(() =>import('pages/OurFriendsPage/OurFriendsPage'));
-// const NewsPage = lazy(() => import('pages/NewsPage/NewsPage'));
+const NewsPage = lazy(() => import('pages/NewsPage/NewsPage'));
 const NoticesPage = lazy(() => import('pages/NoticesPage/NoticesPage'));
 const UserPage = lazy(() => import('pages/UserPage/UserPage'));
 
@@ -18,7 +18,7 @@ const App = () => {
           {/* <Route path="/register" element={<RegisterPage />} /> */}
           {/* <Route path="/login" element={<LoginPage />} /> */}
           {/* <Route path="/friends" element={<OurFriendsPage />} /> */}
-          {/* <Route path="/news" element={<NewsPage />} /> */}
+          <Route path="/news" element={<NewsPage />} />
           <Route path="/notices/:categoryName" element={<NoticesPage />} />
           <Route path="/user" element={<UserPage />} />
         </Route>
