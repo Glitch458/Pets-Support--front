4dcbab39517df3ddd98c91d34af217da2bc3c49e
Andrey Katrych <andrew.katrich@gmail.com>
2022-11-15 19:29:05 +0200

    finish

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,11 +3,9 @@ import { LogoStyle, LogoStyleLetter } from './Logo.styled';
 const Logo = () => {
   return (
     <LogoStyle>
-      {/* <span> */}
       pe
       <LogoStyleLetter>t</LogoStyleLetter>
       ly
-      {/* </span> */}
     </LogoStyle>
   );
 };
