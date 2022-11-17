e23fa6e11302d3549b90e961a2de0ebb0e8bbf41
Andrey Katrych <andrew.katrich@gmail.com>
2022-11-17 15:10:28 +0200

    comment 1 line

--- src/components/FriendsItem/FriendsItem.jsx
+++ src/components/FriendsItem/FriendsItem.jsx
@@ -1,29 +1,43 @@
-import { workDaysCard } from "./workDaysCard"
-import { TitleLink,  FriendCard,Time, Text, MapLink, Img, SponsorItem } from "./FriendsItem.styled"
-import defaultImage from "../../images/defaultImage.jpg";
+// import { workDaysCard } from "./workDaysCard"
+import {
+  TitleLink,
+  FriendCard,
+  Time,
+  Text,
+  MapLink,
+  Img,
+  SponsorItem,
+} from './FriendsItem.styled';
+import defaultImage from '../../images/defaultImage.jpg';
 
-export const FriendsItem = ({ title,
-    siteUrl,
-    mapUrl,
-    imageUrl= defaultImage,
-    adress,
-    workDays,
-    phone,
-    email }) => {
-
-    
-    return (
-        <SponsorItem>
-            <TitleLink href={siteUrl}>{title}</TitleLink>
-            <FriendCard>
-                <Img src={imageUrl ?? defaultImage} alt={title}  />
-                <div>
-                    <Time>Time:  </Time>
-                <MapLink href={mapUrl}>Adress: {adress ?? "------------------"}</MapLink>
-                <Text>Email: <p>{email ?? "------------------"}</p></Text>
-                <Text>Phone: <p>{phone ?? '+38(0XX)XXXXXXX'}</p></Text>
-            </div>
-            </FriendCard>
-        </SponsorItem>
-    )   
-}
+export const FriendsItem = ({
+  title,
+  siteUrl,
+  mapUrl,
+  imageUrl = defaultImage,
+  adress,
+  workDays,
+  phone,
+  email,
+}) => {
+  return (
+    <SponsorItem>
+      <TitleLink href={siteUrl}>{title}</TitleLink>
+      <FriendCard>
+        <Img src={imageUrl ?? defaultImage} alt={title} />
+        <div>
+          <Time>Time: </Time>
+          <MapLink href={mapUrl}>
+            Adress: {adress ?? '------------------'}
+          </MapLink>
+          <Text>
+            Email: <p>{email ?? '------------------'}</p>
+          </Text>
+          <Text>
+            Phone: <p>{phone ?? '+38(0XX)XXXXXXX'}</p>
+          </Text>
+        </div>
+      </FriendCard>
+    </SponsorItem>
+  );
+};
