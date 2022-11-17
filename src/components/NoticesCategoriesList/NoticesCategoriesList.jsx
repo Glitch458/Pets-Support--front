f2d96cabc8b19e9689c9fb30d62c126d0b9070db
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-11-18 01:40:03 +0200

    NoticeModal, modalHelpers

--- src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,6 +1,8 @@
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ImgCover from 'images/pet-cover.jpg';
 import { ButtonLink } from 'components/Button/Button';
+import { useState } from "react";
+import { ModalNotice } from "../ModalNotice/ModatNotice";
 
 import {
   NoticesCategoriesListContainer,
@@ -15,8 +17,17 @@ import {
 } from './NoticesCategoriesList.styled';
 
 const NoticesCategoriesList = ({ category }) => {
-  console.log(category);
+  // console.log(category);
+  const [detailed, setDetailed] = useState(false);
+
+  const toggleModal = () => {
+    setDetailed(prev => {
+      return !prev;
+    });
+  };
+
   return (
+    <>
     <NoticesCategoriesListContainer>
       <NoticesCategoriesListCard>
         <CategoryName>in good hands</CategoryName>
@@ -50,150 +61,12 @@ const NoticesCategoriesList = ({ category }) => {
           </DetailsItem>
         </DetailsList>
         <ButtonContainer>
-          <ButtonLink>Learn more</ButtonLink>
-        </ButtonContainer>
-      </NoticesCategoriesListCard>
-      <NoticesCategoriesListCard>
-        <CategoryName>in good hands</CategoryName>
-        <ButtonLike type="button">
-          <FavoriteBorderIcon
-            sx={{
-              width: 28,
-              height: 28,
-              color: '#F59256',
-            }}
-          />
-        </ButtonLike>
-        <Img src={ImgCover} alt="" />
-        <Title>Cute dog looking for a home</Title>
-        <DetailsList>
-          <DetailsItem key="id1">
-            <span>Breed:</span>
-            Pomeranian
-          </DetailsItem>
-          <DetailsItem key="id2">
-            <span>Place:</span>
-            Lviv
-          </DetailsItem>
-          <DetailsItem key="id3">
-            <span>Age:</span>
-            one year
-          </DetailsItem>
-          <DetailsItem key="id4">
-            <span>Price:</span>
-            50$
-          </DetailsItem>
-        </DetailsList>
-        <ButtonContainer>
-          <ButtonLink>Learn more</ButtonLink>
-        </ButtonContainer>
-      </NoticesCategoriesListCard>
-      <NoticesCategoriesListCard>
-        <CategoryName>in good hands</CategoryName>
-        <ButtonLike type="button">
-          <FavoriteBorderIcon
-            sx={{
-              width: 28,
-              height: 28,
-              color: '#F59256',
-            }}
-          />
-        </ButtonLike>
-        <Img src={ImgCover} alt="" />
-        <Title>Cute dog looking for a homeCute dog looking for a home</Title>
-        <DetailsList>
-          <DetailsItem key="id1">
-            <span>Breed:</span>
-            Pomeranian
-          </DetailsItem>
-          <DetailsItem key="id2">
-            <span>Place:</span>
-            Lviv
-          </DetailsItem>
-          <DetailsItem key="id3">
-            <span>Age:</span>
-            one year
-          </DetailsItem>
-          <DetailsItem key="id4">
-            <span>Price:</span>
-            50$
-          </DetailsItem>
-        </DetailsList>
-        <ButtonContainer>
-          <ButtonLink>Learn more</ButtonLink>
-        </ButtonContainer>
-      </NoticesCategoriesListCard>
-      <NoticesCategoriesListCard>
-        <CategoryName>in good hands</CategoryName>
-        <ButtonLike type="button">
-          <FavoriteBorderIcon
-            sx={{
-              width: 28,
-              height: 28,
-              color: '#F59256',
-            }}
-          />
-        </ButtonLike>
-        <Img src={ImgCover} alt="" />
-        <Title>Cute dog looking for a home</Title>
-        <DetailsList>
-          <DetailsItem key="id1">
-            <span>Breed:</span>
-            Pomeranian
-          </DetailsItem>
-          <DetailsItem key="id2">
-            <span>Place:</span>
-            Lviv
-          </DetailsItem>
-          <DetailsItem key="id3">
-            <span>Age:</span>
-            one year
-          </DetailsItem>
-          <DetailsItem key="id4">
-            <span>Price:</span>
-            50$
-          </DetailsItem>
-        </DetailsList>
-        <ButtonContainer>
-          <ButtonLink>Learn more</ButtonLink>
-        </ButtonContainer>
-      </NoticesCategoriesListCard>
-      <NoticesCategoriesListCard>
-        <CategoryName>in good hands</CategoryName>
-        <ButtonLike type="button">
-          <FavoriteBorderIcon
-            sx={{
-              width: 28,
-              height: 28,
-              color: '#F59256',
-            }}
-          />
-        </ButtonLike>
-        <Img src={ImgCover} alt="" />
-        <Title>Cute dog looking for a home</Title>
-        <DetailsList>
-          <DetailsItem key="id1">
-            <span>Breed:</span>
-            Pomeranian
-          </DetailsItem>
-          <DetailsItem key="id2">
-            <span>Place:</span>
-            Lviv
-          </DetailsItem>
-          <DetailsItem key="id3">
-            <span>Age:</span>
-            one year
-          </DetailsItem>
-          <DetailsItem key="id4">
-            <span>Price:</span>
-            50$
-          </DetailsItem>
-        </DetailsList>
-        <ButtonContainer>
-          <ButtonLink>Learn more</ButtonLink>
+          <ButtonLink onClick ={toggleModal}>Learn more</ButtonLink>
         </ButtonContainer>
       </NoticesCategoriesListCard>
     </NoticesCategoriesListContainer>
+    { detailed && <ModalNotice toggleModal={toggleModal} /> }
+  </>
   );
 };
 export default NoticesCategoriesList;
