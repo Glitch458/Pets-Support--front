7bc0235c860cbbc9a4e046817bae4c986de53fd3
Andrey Katrych <andrew.katrich@gmail.com>
2022-11-18 15:26:58 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from 'redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
+import { /*persistor,*/ store } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './constants/theme';
