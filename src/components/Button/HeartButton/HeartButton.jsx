0490a06c2bca18c25762a9122176b2c338ff11fc
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-11-21 10:38:03 +0200

    some fix

--- src/components/Button/HeartButton/HeartButton.jsx
+++ src/components/Button/HeartButton/HeartButton.jsx
@@ -1,9 +1,9 @@
 import { HeartBtn } from "./HeartButton.styled";
 import IconHeart from "../../../images/icons/Heart.svg";
 
-export const HeartButton = ({ onClick, className, authorized }) => {
+export const HeartButton = ({ onClick, className }) => {
     return (
-        <HeartBtn onClick={onClick} className={className || ""} authorized={authorized}>
+        <HeartBtn onClick={onClick} className={className || ""}>
             <IconHeart />
         </HeartBtn>
     );
