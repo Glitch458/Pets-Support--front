da06d08f5e1dfacab0e4f5271b5f79aa2721bba8
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-11-21 12:16:42 +0200

    do second button and fix first

---
+++ src/components/ModalNotice/ModalNotice.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { HeartBtn } from "components/Button/Button.styled";
+import { ReactComponent as IconHeart } from "../../../src/images/icons/smallHeart.svg";
+
+export const AddToFavorites = () => (
+  <HeartBtn>
+    Add to
+    <IconHeart />
+  </HeartBtn>
+);
--- src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,8 +1,8 @@
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ImgCover from "images/pet-cover.jpg";
 import { ButtonLink } from "components/Button/Button";
-// import { useState } from "react";
-// import { ModalNotice } from "../ModalNotice/ModatNotice";
+import { useState } from "react";
+import { ModalNotice } from "../ModalNotice/ModatNotice";
 
 import {
   NoticesCategoriesContainerList,
@@ -13,33 +13,33 @@ import {
   DetailsItem,
   ButtonContainer,
   CategoryName,
-  ButtonLike,
+  AddToFavorites,
 } from "./NoticesCategoriesList.styled";
 
 const NoticesCategoriesList = ({ data }) => {
   // console.log(category);
-  // const [detailed, setDetailed] = useState(false);
+  const [detailed, setDetailed] = useState(false);
 
-  // const toggleModal = () => {
-  //   setDetailed((prev) => {
-  //     return !prev;
-  //   });
-  // };
+  const toggleModal = () => {
+    setDetailed((prev) => {
+      return !prev;
+    });
+  };
 
   return (
     <NoticesCategoriesContainerList>
       {data.map((item) => (
         <NoticesCategoriesItem key={item._id}>
           <CategoryName>{item.category}</CategoryName>
-          <ButtonLike type="button">
-            <FavoriteBorderIcon
+          <AddToFavorites type="button">
+            {/* <FavoriteBorderIcon
               sx={{
                 width: 28,
                 height: 28,
                 color: "#F59256",
               }}
-            />
-          </ButtonLike>
+            /> */}
+          </AddToFavorites>
           <Img src={ImgCover} alt="" />
           <Title>{item.title}</Title>
           <DetailsList>
@@ -61,10 +61,11 @@ const NoticesCategoriesList = ({ data }) => {
             </DetailsItem>
           </DetailsList>
           <ButtonContainer>
-            <ButtonLink /*onClick={toggleModal}*/>Learn more</ButtonLink>
+            <ButtonLink onClick={toggleModal}>Learn more</ButtonLink>
           </ButtonContainer>
         </NoticesCategoriesItem>
       ))}
+      {detailed && <ModalNotice toggleModal={toggleModal}></ModalNotice>}
     </NoticesCategoriesContainerList>
   );
 };
