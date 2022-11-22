307932a149a45c6037f5ab5fc94e19a7f3204eac
Andrey Katrych <andrew.katrich@gmail.com>
2022-11-22 23:16:20 +0200

    set baseUrl at hosting

--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,33 @@ const SharedLayout = lazy(() => import('components/SharedLayout/SharedLayout'));
 const Homepage = lazy(() => import('pages/Homepage/Homepage'));
 const RegisterPage = lazy(() => import('pages/Register/Register'));
 const LoginPage = lazy(() => import('pages/Login/Login'));
-const OurFriendsPage = lazy(() => import('pages/OurFriendsPage/OurFriendsPage'));
+const OurFriendsPage = lazy(() =>
+  import('pages/OurFriendsPage/OurFriendsPage')
+);
 const NewsPage = lazy(() => import('pages/NewsPage/NewsPage'));
 const NoticesPage = lazy(() => import('pages/NoticesPage/NoticesPage'));
 const UserPage = lazy(() => import('pages/UserPage/UserPage'));
 const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
 
 const App = () => {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(authOperations.fetchCurrentUser())
-  }, [dispatch])
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
 
   return (
     <>
-      {/* <SharedLayout path="/"></SharedLayout> */}
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route path="/" exact element={<Homepage />} />
 
-          <Route path="/register" element={< PublicRoute />}>
+          <Route path="/register" element={<PublicRoute />}>
             <Route index element={<RegisterPage />} />
           </Route>
           <Route path="/login" element={<PublicRoute />}>
             <Route index element={<LoginPage />} />
           </Route>
-
-          {/* <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} /> */}
           <Route path="/friends" element={<OurFriendsPage />} />
           <Route path="/news" element={<NewsPage />} />
           <Route path="/notices/:categoryName" element={<NoticesPage />} />
--- src/redux/Auth/auth-operations.js
+++ src/redux/Auth/auth-operations.js
@@ -1,72 +1,72 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'http://localhost:3002/api/auth/';
+axios.defaults.baseURL = 'https://pets-support.onrender.com/api/auth/';
 
 export const token = {
-    set(token) {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    },
-    unset() {
-        axios.defaults.headers.common.Authorization = '';
-    },
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
 };
 
-const register = createAsyncThunk('auth/register', async (creditals) => {
-    try {
-        console.log(creditals);
-        const { data } = await axios.post('/register', creditals)
-        token.set(data.token)
-        return data
-    } catch (error) {
-        console.log(error);
-    }
+const register = createAsyncThunk('auth/register', async creditals => {
+  try {
+    console.log(creditals);
+    const { data } = await axios.post('/register', creditals);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const logIn = createAsyncThunk('auth/login', async creditals => {
-    try {
-        const { data } = await axios.post('/login', creditals)
-        token.set(data.token)
-        return data
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const { data } = await axios.post('/login', creditals);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const logOut = createAsyncThunk('auth/logout', async () => {
-    try {
-        await axios.post('/logout')
-        token.unset()
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await axios.post('/logout');
+    token.unset();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const fetchCurrentUser = createAsyncThunk(
-    'auth/refresh',
-    async (_, thunkAPI) => {
-        const state = thunkAPI.getState();
-        const persistedToken = state.auth.token;
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
 
-        if (persistedToken === null) {
-            return thunkAPI.rejectWithValue();
-        }
-
-        token.set(persistedToken);
-        try {
-            const { data } = await axios.get('/current');
-            return data;
-        } catch (error) {
-            console.log(error);
-        }
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue();
     }
+
+    token.set(persistedToken);
+    try {
+      const { data } = await axios.get('/current');
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 );
 
 const authOperations = {
-    register,
-    logIn,
-    logOut,
-    fetchCurrentUser,
-}
+  register,
+  logIn,
+  logOut,
+  fetchCurrentUser,
+};
 
 export default authOperations;
